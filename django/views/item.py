3aaeb2db79e4bd072de29360bdaac5396eeaf4e4
jb55 <jb55@bd57cdee-f201-48ba-b5f5-b488ddbd62b2>
2009-07-21 01:02:34 +0000

    infobox rss
    git-svn-id: http://higgr.com/svn/jgblue/trunk@79 bd57cdee-f201-48ba-b5f5-b488ddbd62b2

--- django/views/item.py
+++ django/views/item.py
@@ -5,12 +5,14 @@ def index(request):
 	return render_to_response("item/index.htm")
 
 def detail(request, item_id):
-	quickinfo = ( 
-		{ "label": "Item Number", "value": item_id },
-	)
 	
-	item = { "name": "Item Placeholder",
-			 "quickinfo": quickinfo, }
+	item = { "name": "Item Placeholder", "level": 5}
 			 
-	data = { "item": item }
+	quickinfo = ( 
+		{ "label": "Level", 		"value": item["level"] },
+		{ "label": "Item Number",	"value": item_id },
+		{ "label": "Class", 		"value": "Placeholder Class" },
+	)
+			 
+	data = { "item": item, "quickinfo": quickinfo }
 	return render_to_response("item/detail.htm", data)
