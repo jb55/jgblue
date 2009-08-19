01e8ea6fd4982fb846d03acd31c0749605dc1b19
jb55 <jb55@bd57cdee-f201-48ba-b5f5-b488ddbd62b2>
2009-08-19 18:52:58 +0000

    * working listview with datas
    git-svn-id: http://higgr.com/svn/jgblue/branches/live@128 bd57cdee-f201-48ba-b5f5-b488ddbd62b2

--- static/js/main.js
+++ static/js/main.js
@@ -3,19 +3,50 @@ var jgblue = {};
 
 jgblue.listview = function (options, data) {
 
+	/* 
+	 * The template specifies what columns are needed and what information needs to be in each
+	 * column. The column ids match with item property names, so we should never desync information
+	 * as long as the template is correct
+	 */
 	function buildTable() {
-		var len = _cols.length;
+		var num_cols = _cols.length;
 		var tab = ["<table><tr>"];
 		var col = _cols[0];
-		for(var i=0; i < len; ++i, col=_cols[i]) {
-			tab.push("<th>", col.name,"</th>");
+		
+		/* build column headers from the template */
+		for(var i=0; i < num_cols; ++i, col=_cols[i]) {
+			tab.push("<th style=\"width:", col.width,";text-align:", 
+					 	col.align,";\">", col.name,"</th>");
 		}
-		tab.push("</tr></table>");
-		$(_parent).append(tab.join(""));
+		tab.push("</tr>");
+
+		var url = "file:///C:/Users/Jb55/dev/jgblue/trunk/sandbox/data.js";
+		
+		/* our super-cool web 2.0 json XMLHttpRequest, also known as ajax to all the cool kids */
+		$.getJSON(url, function(data) {								
+			var num_items = data.items.length;
+			var items = data.items;
+			
+			/* load all items and put their data into their respective columns */
+			for(var i=0, item=items[0]; i < num_items; ++i, item=items[i]) {
+				tab.push("<tr>");
+				for(var j=0, col=_cols[0]; j < num_cols; ++j, col=_cols[j]) {
+					tab.push("<td style=\"text-align:",col.align,"\">", item[col.id], "</td>");
+				}
+				tab.push("</tr>");
+			}
+			
+			tab.push("</table>");
+			$(_parent).append(tab.join(""));
+			/* I'm done here --Snake */
+		});
+		
+		/* continue loading page, getJSON is async 
+		 * so we can't assume it's done at this point 
+		 */
 	};
 	
-	var _templates = jgblue.listview.templates;
-	var _template = _templates[options.template];
+	var _template = jgblue.listview.templates[options.template];
 	var _cols = _template.columns;
 	var _parent = options.parent;
 	var _data = data;
@@ -31,9 +62,9 @@ jgblue.listview.templates = {
 	items: {
 		columns: [
 			{id: "name", name: "Name", type: "text", align: "left", width: "40%"},
-			{id: "level", name: "Level", type: "number", align: "left", width: "10%"},
-			{id: "rank", name: "Rank", type: "number", align: "left", width: "10%"},
-			{id: "item_class", name: "Class", type: "number", align: "left", width: "30%"},
+			{id: "level", name: "Level", type: "number", align: "center", width: "10%"},
+			/*{id: "level", name: "Rank", type: "number", align: "center", width: "10%"},*/
+			{id: "item_class", name: "Class", type: "number", align: "center", width: "30%"},
 		],
 	},
 };
