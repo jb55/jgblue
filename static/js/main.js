477c9a4717c64564bd7ab274c0b89927bb02efdd
jb55 <jb55@bd57cdee-f201-48ba-b5f5-b488ddbd62b2>
2009-08-20 00:32:27 +0000

    * item links, updated hyperlink style
    git-svn-id: http://higgr.com/svn/jgblue/trunk@143 bd57cdee-f201-48ba-b5f5-b488ddbd62b2

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,21 @@
 // JGBlue.com
 var jgblue = {};
 
+function compute_obj_link(data) {
+	return ["<a href=\"",data.template.id,"/",data.item.id,"\">",data.item[data.field],"</a>"].join('');
+}
+
 jgblue.listview = function (options, data) {
+
+
+
 
 	/* 
 	 * The template specifies what columns are needed and what information needs to be in each
 	 * column. The column ids match with item property names, so we should never desync information
 	 * as long as the template is correct
 	 */
-	function buildTable() {
+	function build_table() {
 		var num_cols = _cols.length;
 		var tab = ["<table width=\"100%\"><tr>"];
 		var col = _cols[0];
@@ -31,7 +38,8 @@ jgblue.listview = function (options, data) {
 			for(var i=0, item=items[0]; i < num_items; ++i, item=items[i]) {
 				tab.push("<tr>");
 				for(var j=0, col=_cols[0]; j < num_cols; ++j, col=_cols[j]) {
-					tab.push("<td style=\"text-align:",col.align,"\">", item[col.id], "</td>");
+					var val = col.compute == undefined ? item[col.id] : col.compute({template: _template, item:item, field: col.id});
+					tab.push("<td style=\"text-align:",col.align,"\">", val, "</td>");
 				}
 				tab.push("</tr>");
 			}
@@ -52,7 +60,7 @@ jgblue.listview = function (options, data) {
 	var _data = data;
 
 	
-	buildTable();
+	build_table();
 };
 
 jgblue.listview.templates = {
@@ -60,8 +68,9 @@ jgblue.listview.templates = {
 	// Item listview template
 	// --------------------------------
 	items: {
+		id: "item",
 		columns: [
-			{id: "name", name: "Name", type: "text", align: "left", width: "40%"},
+			{id: "name", name: "Name", type: "text", align: "left", width: "40%", compute:compute_obj_link},
 			{id: "level", name: "Level", type: "number", align: "center", width: "10%"},
 			/*{id: "level", name: "Rank", type: "number", align: "center", width: "10%"},*/
 			{id: "item_class", name: "Class", type: "number", align: "center", width: "30%"},
