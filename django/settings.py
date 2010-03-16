d2769f85f839f3f1c2649f20288e76681315ede9
Bill Casarin <jb@jb55.com>
2010-03-16 02:58:42 +0000

    Debugging enabled

--- django/settings.py
+++ django/settings.py
@@ -11,7 +11,7 @@ PROJECT_DIR = "/home/jb55/jgblue-dev"
 STATIC_DIR = os.path.join(PROJECT_DIR, "static")
 TEMPLATES_DIR = os.path.join(PROJECT_DIR, "django/templates")
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
